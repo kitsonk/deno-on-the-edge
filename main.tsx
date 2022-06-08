8f0112315f14c4a8330c97b89113ff82a2cae898
Kitson Kelly <me@kitsonkelly.com>
2022-06-08 09:07:47 +0000

    stuff

--- main.tsx
+++ main.tsx
@@ -1,9 +1,21 @@
 /** @jsx h */
 /** @jsxFrag Fragment */
-import { Fragment, init, h, render, tw } from "nat";
+import { Fragment, h, init, render, tw } from "nat";
+
+const res = await fetch("https://apiland.deno.dev/v2/modules");
+const pkgs: { items: { name: string; description: string }[] } = await res
+  .json();
 
 function App() {
-  return <h1 class={tw`text-4xl`}>Hello from nat!</h1>;
+  const items = pkgs.items.map(({ name, description }) => (
+    <li>{name} - {description}</li>
+  ));
+  return (
+    <div>
+      <h1 class={tw`text-4xl`}>Hello from nat!</h1>
+      <ul class={tw`list`}>{items}</ul>
+    </div>
+  );
 }
 
 const router = init();
